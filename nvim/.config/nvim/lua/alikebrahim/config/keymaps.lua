5762033c6bdf3203258b696e9a46ed99d4667276
aliebrahim <alikebrahim@me.com>
2025-04-27 21:42:19 +0300

    nvim

--- nvim/.config/nvim/lua/alikebrahim/config/keymaps.lua
+++ nvim/.config/nvim/lua/alikebrahim/config/keymaps.lua
@@ -3,8 +3,21 @@ local api = vim.api
 local opts = { noremap = true, silent = true }
 
 -- [[ Basic Keymaps ]]
--- Easier escape to normal mode from insert mode
--- keymap.set("i", "jj", "<Esc>")
+
+-- Clear highlights on search when pressing <Esc> in normal mode
+--  See `:help hlsearch`
+keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+-- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
+-- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
+-- is not what someone will guess without a bit more experience.
+--
+-- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
+-- or just use <C-\><C-n> to exit terminal mode
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
 -- Move lines in visual mode
 keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
@@ -48,8 +61,8 @@ keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true }
 keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 -- Diagnostic keymaps
-keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
-keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+-- keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' }) --deprecated
+-- keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' }) --deprecated
 keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 -- [[ end Kickstart.nvim ]]
--- nvim/.config/nvim/lua/alikebrahim/config/options.lua
+++ nvim/.config/nvim/lua/alikebrahim/config/options.lua
@@ -1,3 +1,4 @@
+--  For more options, you can see `:help option-list`
 vim.g.have_nerd_font = true
 vim.g.netrw_banner = 0
 
@@ -17,22 +18,28 @@ opt.breakindent = true -- Enable break indent
 opt.incsearch = true -- Case-insensitive searching UNLESS \C or capital in search
 opt.ignorecase = true
 opt.smartcase = true
+
 opt.hlsearch = false -- Set highlight on search
---
+
 -- Appearance
 opt.number = true -- Make line numbers default
 opt.relativenumber = true -- Make relative line numbers default
+
 opt.termguicolors = true
 opt.cmdheight = 1
-opt.scrolloff = 10
+
+opt.scrolloff = 10 -- Minimal number of screen lines to keep above and below the cursor.
+
 opt.completeopt = 'menuone,noinsert,noselect' -- Better completion experience
+
 opt.signcolumn = 'yes' -- Keep signcolumn on by default
-opt.list = true
-opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+opt.list = true -- Sets how neovim will display certain whitespace characters in the editor.
+opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' } --  See `:help 'list'` and `:help 'listchars'`
 
 -- Behaviour
 opt.updatetime = 250 -- Decrease update time
-opt.timeoutlen = 300
+opt.timeoutlen = 300 -- Decrease mapped sequence wait time
 opt.hidden = true
 opt.errorbells = false
 opt.swapfile = false
@@ -40,14 +47,27 @@ opt.backup = false
 opt.undodir = vim.fn.expand '~/.vim/undodir'
 opt.undofile = true -- Save undo history
 opt.backspace = 'indent,eol,start'
-opt.splitright = true
+
+opt.splitright = true -- Configure how new splits should be opened
 opt.splitbelow = true
+
 opt.autochdir = false
 opt.iskeyword:append '-'
 opt.mouse = 'a' -- Enable mouse mode
 opt.clipboard = 'unnamedplus' -- Sync clipboard between OS and Neovim.
 opt.modifiable = true
 opt.encoding = 'UTF-8'
-opt.showmode = false
+opt.showmode = false -- Don't show the mode, since it's already in the status line
 opt.inccommand = 'split' -- Preview substitutions live
-opt.cursorline = false -- Show which line the cursor is on
+opt.cursorline = true -- Show which line the cursor is on
+
+-- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
+-- instead raise a dialog asking if you wish to save the current file(s)
+-- See `:help 'confirm'`
+vim.opt.confirm = true
+
+-- Sync clipboard between OS and Neovim.
+--  Schedule the setting after `UiEnter` because it can increase startup-time.
+vim.schedule(function()
+  vim.opt.clipboard = 'unnamedplus'
+end)
